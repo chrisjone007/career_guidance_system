0e0ae0387a35ab4b2e63491c711c2519329750e6
Akhumeya Abiodun <chrisjone007@gmail.com>
2026-03-29 09:49:04 +0100

    Update API config for production

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,8 +1,3 @@
-// src/api/config.js
-
-// During development, use localhost. After deployment, use Render.
 const API_BASE_URL = import.meta.env.MODE === 'development' 
-    ? 'http://localhost:5173/' 
-    : 'https://faculty-api-pdud.onrender.com';
-
-export default API_BASE_URL;
+    ? 'http://127.0.0.1:8000' 
+    : 'https://career-api.onrender.com'; // Paste your Render backend link here
